d35202862633ba9c0fba28b088b399431f108ab3
kkyuri0419 <kkyuri0419@gmail.com>
2019-11-21 11:04:51 +0900

    11/21 목요일 아침

--- app/src/main/java/com/example/moment/DiaryMakeNew.java
+++ app/src/main/java/com/example/moment/DiaryMakeNew.java
@@ -1,10 +1,12 @@
 package com.example.moment;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -112,6 +114,15 @@ public class DiaryMakeNew extends AppCompatActivity {
 
         alertDialog.show();
         alertDialog.getWindow().setLayout(800,500);
+    }
+
+
+    //갤러리에서 사진 불러오기
+    private void takePhotoFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE);
     }
 
 
